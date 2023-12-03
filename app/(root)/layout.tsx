3cfe3ef87fe233a7a0209f36b1d9b21c9d0172e2
Aleksey Verin <verevaa@gmail.com>
2023-12-03 19:01:57 +0300

    feat: added navbar and theme toggle

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,25 @@
-import { PropsWithChildren } from "react";
+import { Navbar } from "@/components/navbar";
+// import { Sidebar } from "@/components/sidebar";
+// import { checkSubscription } from "@/lib/subscription";
 
-const RootLayout = ({children}: PropsWithChildren) => {
-  return ( <div className=''>
-    {children}
-  </div> );
+const RootLayout = async ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  // const isPro = await checkSubscription();
+
+  return ( 
+    <div className="h-full">
+      <Navbar isPro={false} />
+      <div className="hidden md:flex mt-16 h-full w-20 flex-col fixed inset-y-0">
+        {/* <Sidebar isPro={isPro} /> */}
+      </div>
+      <main className="md:pl-20 pt-16 h-full">
+        {children}
+      </main>
+    </div>
+   );
 }
  
 export default RootLayout;
